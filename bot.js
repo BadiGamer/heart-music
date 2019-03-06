bfbcf26657549405752229c3f95a46e27b4d3b06
BadiGamer <48218842+BadiGamer@users.noreply.github.com>
2019-03-06 18:59:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity(" 1help",{type: 'use'})
+   client.user.setActivity("",{type: '1help'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
